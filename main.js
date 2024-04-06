588d25ec64109f816b4cfc3a791aace57659faee
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-04-06 17:06:15 -0300

    js

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form-number');
+const numberA = document.getElementById('number-a');
+const numberB = document.getElementById('number-b');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mensagemErro = `O número B deve ser maior que o número A`;
+    const mensagemSucesso = `Parabéns! <b>${numberA.value}</b> é menor que <b>${numberB.value}</b>, validação completa`;
+
+    if(numberA.value >= numberB.value) {
+        numberA.style.border = '1px solid #FF0000';
+        const containerMensagemErro = document.querySelector('.mensagem-erro').innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+    }else {
+        numberA.style.border = '';
+        numberA.value = '';
+        numberB.value = '';
+        const containerMensagemErro = document.querySelector('.mensagem-erro').innerHTML = '';
+        const containerMesagemSucesso = document.querySelector('.mensagem-sucesso').innerHTML = mensagemSucesso;
+        containerMesagemSucesso.style.display = 'block';
+    }
+});
