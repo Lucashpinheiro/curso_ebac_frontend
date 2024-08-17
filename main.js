41492edd6adafe4c9f71f8b413e3b5103b427050
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-08-16 21:17:27 -0300

    OO

---
+++ main.js
@@ -0,0 +1,43 @@
+function Instrumento(nome){
+    this.nome = nome;
+    this.venda = function(){
+        console.log(this.nome + " A venda no site");
+    }
+}
+
+function Cordas(nome, quantasCordas, acustico, eletrico){
+    this.nome = nome;
+    this.quantasCordas = quantasCordas;
+    this.acustico = acustico;
+    this.eletrico = eletrico;
+
+    Instrumento.call(this, nome);
+}
+
+function Percusao(nome, ferragens, peles, tambores, pecasAdicionais) {
+    this.nome = nome;
+    this.ferragens = ferragens;
+    this.peles = peles;
+    this.tambores = tambores;
+    this.pecasAdicionais = pecasAdicionais;
+
+    Instrumento.call(this, nome);
+
+}
+
+function Sopro(nome, madeira, palheta, embocadura) {
+    this.nome = nome;
+    this.madeira= madeira;
+    this.palheta = palheta;
+    this.embocadura = embocadura;
+
+    Instrumento.call(this, nome);
+}
+
+const instumento1 = new Cordas("Guitarra", 7, false, true);
+const instumento2 = new Percusao("Bateria", true, true, 6, true);
+const instrumento3 = new Sopro("Flauta", false, "Simples", "Fechadas");
+
+console.log(instumento1);
+console.log(instumento2);
+console.log(instrumento3);
