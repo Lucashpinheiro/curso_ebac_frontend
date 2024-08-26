e7cbed02a6c41494bfa2b4516916569bfcff1029
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-08-26 07:21:32 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const alunos = [
+    {
+        nome: 'Lucas',
+        nota: 7
+    },
+    {
+        nome: 'Giulia', 
+        nota: 9
+    },
+    {
+        nome: 'Antonio',
+        nota: 2
+    }
+];
+
+const notaSeis = alunos.forEach(function(item) {
+    if(item.nota >= 6) {
+        console.log(item);
+    }
+})
