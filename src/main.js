64a5ca480fb9019158fec0d8b0b6f3222ca2071a
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-09-01 11:50:10 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    console.log(a * b);
+}
+multiplicar(5, 4);
+function saudação(nome) {
+    console.log('Olá ' + nome);
+}
+saudação('Antonio');
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number) {
+    console.log(a * b);
+}
+multiplicar(5, 4);
+
+function saudação(nome: string) {
+    console.log('Olá ' + nome);
+}
+
+saudação('Antonio');
