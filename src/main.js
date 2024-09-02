d29ecc797f68ffc0f9f63f92b43c98328cdde672
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2024-09-02 20:00:36 -0300

    ajustando codigo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 "use strict";
 function multiplicar(a, b) {
-    console.log(a * b);
+    return console.log(a * b);
 }
 multiplicar(5, 4);
 function saudação(nome) {
-    console.log('Olá ' + nome);
+    return console.log('Olá ' + nome);
 }
 saudação('Antonio');
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 function multiplicar(a: number, b: number) {
-    console.log(a * b);
+    return console.log(a * b);
 }
 multiplicar(5, 4);
 
+
 function saudação(nome: string) {
-    console.log('Olá ' + nome);
+    return console.log('Olá ' + nome);
 }
 
 saudação('Antonio');
